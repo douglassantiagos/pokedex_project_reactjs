4551dd971ef136c4164380a9569f961547fba009
douglassantiagos <douglassantiagods@hotmail.com>
2022-08-08 20:40:24 -0300

    improving performance

--- src/utils/typeColors.tsx
+++ src/utils/typeColors.tsx
@@ -2,81 +2,81 @@ export function typeColors(type) {
     let primary = '';
     let secondary = '';
 
-    // if (type[0] === 'normal') {
-    //     primary = 'orange.400'
-    // } else if (type[0] === 'fighting') {
-    //     primary = 'orange.200'
-    // } else if (type[0] === 'flying') {
-    //     primary = 'red.200'    
-    // } else if (type[0] === 'poison') {
-    //     primary = 'purple.400'
-    // } else if (type[0] === 'ground') {
-    //     primary = 'yellow.500'
-    // } else if (type[0] === 'rock') {
-    //     primary = 'gray.400'
-    // } else if (type[0] === 'bug') {
-    //     primary = 'green.300'
-    // } else if (type[0] === 'ghost') {
-    //     primary = 'purple.300'
-    // } else if (type[0] === 'steel') {
-    //     primary = 'gray.300'
-    // } else if (type[0] === 'fire') {
-    //     primary = 'red.400'
-    // } else if (type[0] === 'water') {
-    //     primary = 'blue.400'
-    // } else if (type[0] === 'grass') {
-    //     primary = 'green.400'
-    // } else if (type[0] === 'electric') {
-    //     primary = 'yellow.400'
-    // } else if (type[0] === 'psychic') {
-    //     primary = 'purple.200'
-    // } else if (type[0] === 'ice') {
-    //     primary = 'cyan.300'
-    // } else if (type[0] === 'dragon') {
-    //     primary = 'orange'
-    // } else if (type[0] === 'dark') {
-    //     primary = 'blue.600'
-    // } else if (type[0] === 'fairy') {
-    //     primary = 'pink.300'
-    // }
+    if (type[0] === 'normal') {
+        primary = 'orange.400'
+    } else if (type[0] === 'fighting') {
+        primary = 'orange.200'
+    } else if (type[0] === 'flying') {
+        primary = 'red.200'    
+    } else if (type[0] === 'poison') {
+        primary = 'purple.400'
+    } else if (type[0] === 'ground') {
+        primary = 'yellow.500'
+    } else if (type[0] === 'rock') {
+        primary = 'gray.400'
+    } else if (type[0] === 'bug') {
+        primary = 'green.300'
+    } else if (type[0] === 'ghost') {
+        primary = 'purple.300'
+    } else if (type[0] === 'steel') {
+        primary = 'gray.300'
+    } else if (type[0] === 'fire') {
+        primary = 'red.400'
+    } else if (type[0] === 'water') {
+        primary = 'blue.400'
+    } else if (type[0] === 'grass') {
+        primary = 'green.400'
+    } else if (type[0] === 'electric') {
+        primary = 'yellow.400'
+    } else if (type[0] === 'psychic') {
+        primary = 'purple.200'
+    } else if (type[0] === 'ice') {
+        primary = 'cyan.300'
+    } else if (type[0] === 'dragon') {
+        primary = 'orange'
+    } else if (type[0] === 'dark') {
+        primary = 'blue.600'
+    } else if (type[0] === 'fairy') {
+        primary = 'pink.300'
+    }
 
-    // if (type[1] === 'normal') {
-    //     secondary = 'orange.400'
-    // } else if (type[1] === 'fighting') {
-    //     secondary = 'orange.200'
-    // } else if (type[1] === 'flying') {
-    //     secondary = 'red.200'    
-    // } else if (type[1] === 'poison') {
-    //     secondary = 'purple.400'
-    // } else if (type[1] === 'ground') {
-    //     secondary = 'yellow.500'
-    // } else if (type[1] === 'rock') {
-    //     secondary = 'gray.400'
-    // } else if (type[1] === 'bug') {
-    //     secondary = 'green.300'
-    // } else if (type[1] === 'ghost') {
-    //     secondary = 'purple.300'
-    // } else if (type[1] === 'steel') {
-    //     secondary = 'gray.300'
-    // } else if (type[1] === 'fire') {
-    //     secondary = 'red.400'
-    // } else if (type[1] === 'water') {
-    //     secondary = 'blue.400'
-    // } else if (type[1] === 'grass') {
-    //     secondary = 'green.400'
-    // } else if (type[1] === 'electric') {
-    //     secondary = 'yellow.400'
-    // } else if (type[1] === 'psychic') {
-    //     secondary = 'purple.200'
-    // } else if (type[1] === 'ice') {
-    //     secondary = 'cyan.300'
-    // } else if (type[1] === 'dragon') {
-    //     secondary = 'orange'
-    // } else if (type[1] === 'dark') {
-    //     secondary = 'blue.600'
-    // } else if (type[1] === 'fairy') {
-    //     secondary = 'pink.300'
-    // }
+    if (type[1] === 'normal') {
+        secondary = 'orange.400'
+    } else if (type[1] === 'fighting') {
+        secondary = 'orange.200'
+    } else if (type[1] === 'flying') {
+        secondary = 'red.200'    
+    } else if (type[1] === 'poison') {
+        secondary = 'purple.400'
+    } else if (type[1] === 'ground') {
+        secondary = 'yellow.500'
+    } else if (type[1] === 'rock') {
+        secondary = 'gray.400'
+    } else if (type[1] === 'bug') {
+        secondary = 'green.300'
+    } else if (type[1] === 'ghost') {
+        secondary = 'purple.300'
+    } else if (type[1] === 'steel') {
+        secondary = 'gray.300'
+    } else if (type[1] === 'fire') {
+        secondary = 'red.400'
+    } else if (type[1] === 'water') {
+        secondary = 'blue.400'
+    } else if (type[1] === 'grass') {
+        secondary = 'green.400'
+    } else if (type[1] === 'electric') {
+        secondary = 'yellow.400'
+    } else if (type[1] === 'psychic') {
+        secondary = 'purple.200'
+    } else if (type[1] === 'ice') {
+        secondary = 'cyan.300'
+    } else if (type[1] === 'dragon') {
+        secondary = 'orange'
+    } else if (type[1] === 'dark') {
+        secondary = 'blue.600'
+    } else if (type[1] === 'fairy') {
+        secondary = 'pink.300'
+    }
 
     const colors = {
         primary,
